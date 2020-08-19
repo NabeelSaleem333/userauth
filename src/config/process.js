9061696c35f0dd57c28e7317e38385164999fa9c
NabeelSaleem333 <nabeel.saleem333@gmail.com>
2020-08-19 11:11:34 +0500

    connected with online mongodb

--- src/config/process.js
+++ src/config/process.js
@@ -3,6 +3,7 @@ const { connection } = require("mongoose");
 const devConfig = {
     PORT: process.env.PORT || 4000,
     connectionString: 'mongodb://localhost/userauth',
+    onlinedb: 'mongodb+srv://medical:express@medicalexpresscluster-hkv5p.mongodb.net/userauth?retryWrites=true&w=majority',
     secretKey: 'gmail'
 }
 module.exports = devConfig;
